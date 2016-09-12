e5707a40326279e2409027e4af1904ec6676e834
Mikko Lakomaa <hopealusikka@gmail.com>
2016-09-12 22:08:50 +0300

    Changed: Rename init.js to variables.js

--- src/js/content-script/init.js
+++
@@ -1,5 +0,0 @@
-export const PREFIX = 'galleryzer_';      //Prefix to avoid clashing classes etc
-export const galleryImageClass = PREFIX + 'gallery_image';
-export let content;
-export let notifications;
-export let desiredHeight;
